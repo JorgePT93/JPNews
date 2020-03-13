eba893a606dbc23378ec650886e7a11138690bc9
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-03-13 02:08:26 -0300

    chore: Updated MainApplication.java

--- app/src/main/java/cl/ucn/disc/dsm/jpnews/MainApplication.java
+++ app/src/main/java/cl/ucn/disc/dsm/jpnews/MainApplication.java
@@ -14,15 +14,19 @@
 package cl.ucn.disc.dsm.jpnews;
 
 import android.app.Application;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
+import android.os.StrictMode.VmPolicy;
 import androidx.appcompat.app.AppCompatDelegate;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.decoder.SimpleProgressiveJpegConfig;
+import com.jakewharton.threetenabp.AndroidThreeTen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * The Main Application
- *
- * @author Diego Urrutia-Astorga.
- */
+
+
 public class MainApplication extends Application {
 
   /**
@@ -31,15 +35,51 @@ public class MainApplication extends Application {
   private static final Logger log = LoggerFactory.getLogger(MainApplication.class);
 
   /**
-   * Called when the application is starting, before any activity, service, or receiver objects (excluding content
-   * providers) have been created.
+   * Called when the application is starting, before any activity, service, or receiver objects
+   * (excluding content providers) have been created.
    */
   @Override
   public void onCreate() {
     super.onCreate();
+    log.debug("Initializing ..");
+
+    // Fresco configuration for large images
+
+    ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this)
+        .setProgressiveJpegConfig(new SimpleProgressiveJpegConfig())
+        .setResizeAndRotateEnabledForNetwork(true)
+        .setDownsampleEnabled(true)
+        .build();
+
+    // Fresco initialization
+    Fresco.initialize(this, config);
 
     // Day and Night support
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+
+    // AndroidThreeTen initialization
+    AndroidThreeTen.init(this);
+
+    if (BuildConfig.DEBUG) {
+
+      StrictMode.setThreadPolicy(new ThreadPolicy.Builder()
+          .detectDiskReads()
+          .detectDiskWrites()
+          .detectNetwork()
+          .penaltyLog()
+          .penaltyFlashScreen()
+          .build());
+
+      StrictMode.setVmPolicy(new VmPolicy.Builder()
+          .detectLeakedSqlLiteObjects()
+          .detectLeakedClosableObjects()
+          .detectLeakedRegistrationObjects()
+          .detectActivityLeaks()
+          .detectCleartextNetwork()
+          .penaltyLog()
+          .build());
+
+    }
 
     log.debug("Initializing: Done.");
   }
